f0caa492969784c24397d74195728af19611560f
ZAIN UL ABADIN <zaincomsatsvhr@gmail.com>
2024-05-03 17:32:04 +0500

    use of refactored code || Lecture 57 || Part 04

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -19,14 +20,23 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: new containerRepeted(colors: Color(0xFF1D1E33),
+                  child: ContainerRepeated(
+                    colors: Color(0xFF1D1E33),
+                    cardWidget: RepeatedIcon(
+                      iconData: FontAwesomeIcons.male,
+                      label: 'Male',
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: new containerRepeted(colors: Color(0xFF1D1E33),
+                  child: ContainerRepeated(
+                    colors: Color(0xFF1D1E33),
+                    cardWidget: RepeatedIcon(
+                      iconData: FontAwesomeIcons.female,
+                      label: 'Female',
+                    ),
                   ),
-                  ),
-
+                ),
               ],
             ),
           ),
@@ -34,17 +44,25 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child:  new containerRepeted(colors: Colors.blue),
-
+                  child: ContainerRepeated(
+                    colors: Colors.blue,
+                    cardWidget: Text('Some Widget'),
+                  ),
                 ),
                 Expanded(
-                  child:  new containerRepeted(colors: Color(0xFF1D1E33),),
+                  child: ContainerRepeated(
+                    colors: Color(0xFF1D1E33),
+                    cardWidget: Text('Another Widget'),
+                  ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child:  new containerRepeted(colors: Color(0xFF1D1E33),),
+            child: ContainerRepeated(
+              colors: Color(0xFF1D1E33),
+              cardWidget: Text('Single Widget'),
+            ),
           ),
         ],
       ),
@@ -52,15 +70,46 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class containerRepeted extends StatelessWidget {
-  containerRepeted({required this.colors});
-  final Color colors;
+class RepeatedIcon extends StatelessWidget {
+  RepeatedIcon({required this.iconData, required this.label});
 
+  final IconData iconData;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ContainerRepeated extends StatelessWidget {
+  ContainerRepeated({required this.colors, required this.cardWidget});
+  final Color colors;
+  final Widget cardWidget;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardWidget,
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
